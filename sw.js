27d6efe9d66c9301ecdef9c2e953d5b93ff52dfd
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-13 20:21:05 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'technics-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './img/Technics_logo.png',
+  './img/favicon_512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
